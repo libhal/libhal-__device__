eb60883d65a32b52400a1a53fa7a9a7c190c1e57
Khalil Estell <kammce0@gmail.com>
2024-03-04 13:03:54 -0800

    :recycle: Replace boilerplate w/ libhal-bootstrap

--- conanfile.py
+++ conanfile.py
@@ -13,10 +13,6 @@
 # limitations under the License.
 
 from conan import ConanFile
-from conan.tools.cmake import CMake, cmake_layout
-from conan.tools.files import copy
-from conan.tools.build import check_min_cppstd
-import os
 
 
 required_conan_version = ">=2.0.14"
@@ -30,61 +26,15 @@ class libhal___device___conan(ConanFile):
     description = ("A collection of drivers for the __device__")
     topics = ("__device__", "libhal", "driver")
     settings = "compiler", "build_type", "os", "arch"
-    exports_sources = ("include/*", "tests/*", "LICENSE", "CMakeLists.txt",
-                       "src/*")
-    generators = "CMakeToolchain", "CMakeDeps"
 
-    @property
-    def _min_cppstd(self):
-        return "20"
-
-    @property
-    def _compilers_minimum_version(self):
-        return {
-            "gcc": "11",
-            "clang": "14",
-            "apple-clang": "14.0.0"
-        }
-
-    def validate(self):
-        if self.settings.get_safe("compiler.cppstd"):
-            check_min_cppstd(self, self._min_cppstd)
-
-    def build_requirements(self):
-        self.tool_requires("cmake/3.27.1")
-        self.tool_requires("libhal-cmake-util/4.0.2")
-
-        self.test_requires("boost-ext-ut/1.1.9")
-        self.test_requires("libhal-mock/[^3.0.0]")
+    python_requires = "libhal-bootstrap/[^0.0.3]"
+    python_requires_extend = "libhal-bootstrap.library"
 
     def requirements(self):
-        self.requires("libhal/[^3.0.0]", transitive_headers=True)
-        self.requires("libhal-util/[^4.0.0]")
-
-    def layout(self):
-        cmake_layout(self)
-
-    def build(self):
-        cmake = CMake(self)
-        cmake.configure()
-        cmake.build()
-
-    def package(self):
-        copy(self,
-             "LICENSE",
-             dst=os.path.join(self.package_folder, "licenses"),
-             src=self.source_folder)
-        copy(self,
-             "*.h",
-             dst=os.path.join(self.package_folder, "include"),
-             src=os.path.join(self.source_folder, "include"))
-        copy(self,
-             "*.hpp",
-             dst=os.path.join(self.package_folder, "include"),
-             src=os.path.join(self.source_folder, "include"))
-
-        cmake = CMake(self)
-        cmake.install()
+        # Adds libhal and libhal-util as transitive headers, meaning library
+        # consumers get the libhal and libhal-util headers downstream.
+        bootstrap = self.python_requires["libhal-bootstrap"]
+        bootstrap.module.add_library_requirements(self)
 
     def package_info(self):
         self.cpp_info.libs = ["libhal-__device__"]
